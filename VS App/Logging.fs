3dc7947fde5e61791d04c159f2b297230edb61ef
Alessandro <alessandro.piccione.75@gmail.com>
2018-10-27 20:39:06 +0100

    Logging. Set a new log for every run.

--- VS App/Logging.fs
+++ VS App/Logging.fs
@@ -3,14 +3,14 @@
 open log4net
 open log4net.Appender
 open log4net.Config
-open System.IO
 
 // http://blog.ctaggart.com/2016/11/google-logging-from-log4net.html
 // the example is obviously not working
 
-let configLogger() = 
+let configLogger(logFile) = 
 
-    Util.LogLog.InternalDebugging <- true;
+    // set errors visible
+    //Util.LogLog.InternalDebugging <- true;  //
     let hierarchy = LogManager.GetRepository(System.Reflection.Assembly.GetCallingAssembly()) :?> Repository.Hierarchy.Hierarchy
     hierarchy.Root.Level <- Core.Level.Info
 
@@ -20,7 +20,7 @@ let configLogger() =
     //hierarchy.Root.AddAppender traceAppender
 
     let resultAppender = Appender.RollingFileAppender()
-    resultAppender.File <- "C:\\Logs\\Autotrader.log"    
+    resultAppender.File <- logFile    
     resultAppender.AppendToFile <- true
     resultAppender.Layout <- Layout.PatternLayout("%date %-5level [%2thread] %message%newline")
     resultAppender.ImmediateFlush <- false
@@ -29,8 +29,8 @@ let configLogger() =
     hierarchy.Root.AddAppender resultAppender
 
 
-    //hierarchy.Configured <- true
+    hierarchy.Configured <- true
 
-    BasicConfigurator.Configure hierarchy |> ignore  // half a day spent to find out that nothing happen without this call -> Fucking Log4Net !
+    BasicConfigurator.Configure hierarchy |> ignore
 
     
--- VS App/Program.fs
+++ VS App/Program.fs
@@ -1,15 +1,32 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open System
+open System.IO
+open System.Reflection
+
 
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    let version = 0.1
 
-    Logging.configLogger()
+    printfn "CryptoAutotrader v %f" version
 
-    let logger = log4net.LogManager.GetLogger( System.Reflection.Assembly.GetExecutingAssembly(), "root")
-    logger.Info("Start")
+    let logFile = sprintf @"logs/Autotrader (%s).log" (DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss"))
+    Logging.configLogger(Path.Combine( __SOURCE_DIRECTORY__, logFile))
+
+    let logger = log4net.LogManager.GetLogger( Assembly.GetExecutingAssembly(), "root")
+    logger.Info (sprintf "Start CryptoAutotrader v %f" version)
+
+    // do the job here
+
+    logger.Info "End"
+
 
     System.Threading.Thread.Sleep(10*1000) // wait 10 seconds for logging finish writing
 
-    0 // return an integer exit code
+    #if INTERACTIVE  // when is it considered INTERAVCTIVE ?
+    System.Threading.Thread.Sleep(10*1000) // wait 10 seconds for logging finish writing
+    #endif    
+
+    logger.Logger.Repository.Shutdown()   // flush the log
+
+    0
