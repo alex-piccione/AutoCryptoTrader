c1ed3c900099d3715fdffc9eaf289789f5c72825
Alessandro <alessandro.piccione.75@gmail.com>
2019-05-19 22:28:36 +0100

    Add call to GetBalance in Engine. Add the Consolas and Arial fonts to the project.

--- UI Application 2/Engine.fs
+++ UI Application 2/Engine.fs
@@ -10,9 +10,12 @@ open Alex75.Cryptocurrencies
 
 type PriceChange = {pair:CurrencyPair; price:decimal}
 
+type BalanceUpdate = { XRP:int; EUR:int; USD:int; BTC:int }
+
 type Engine(bitstampClient: IClient) =
 
     let bitstampTickerChanged  = new Event<Ticker>()   
+    let bitstampBalanceUpdated  = new Event<BalanceUpdate>()   
 
     let updatePrices() =           
         let tickers = bitstampClient.GetTickers([|
@@ -21,12 +24,26 @@ type Engine(bitstampClient: IClient) =
             CurrencyPair.XRP_BTC
             |]) 
 
+        //tickers.Values.First().
+
         Parallel.ForEach(tickers.Values, fun ticker -> bitstampTickerChanged.Trigger(ticker) ) |> ignore 
 
+    let updateBalance() =           
+        let response = bitstampClient.GetBalance()
+        if response.IsSuccess then
+            bitstampBalanceUpdated.Trigger( {
+                XRP=int(response.XRP)
+                EUR=int(response.EUR)
+                USD=int(response.USD)
+                BTC=int(response.BTC)
+                })
     
     member __.startUpdatingUI () =
         let timer = new Timer( fun _ -> updatePrices() )
         timer.Change(0, 5000 ) |> ignore
+
+        let timer_Balance = new Timer( fun _ -> updateBalance())
+        timer_Balance.Change(2000, 60*1000) |> ignore
         
         
         
